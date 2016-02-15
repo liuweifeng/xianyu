a66bce7f350aaece820606081911155febc82f90
刘巍峰 <weifeng3@staff.weibo.com>
2016-02-15 14:18:31 +0800

    firsr commit

---
+++ bin/xianyu.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+var path = require('path');
+var program = require('commander');
+var updateNotifier = require('update-notifier');
+var pkg = require('../package.json');
+
+updateNotifier({
+    pkg
+}).notify();
+
+program
+    .version(pkg.version, '-V --version');
+
+program
+    .command('dev')
+    .alias('watch')
+    .description('开发模式')
+    .option('-p, --port [port]', 'The dev-server port')
+    .option('-m, --module [module]', 'The dev module')
+    .option('-e, --entry [entry]', 'The entry file')
+    .option('-t, --template [template]', 'The template html file')
+    .action(require('../lib/action/dev'))
+    .on('--help', function () {
+        console.log('  Examples:');
+        console.log();
+        console.log('    $ deploy exec sequential');
+        console.log('    $ deploy exec async');
+        console.log();
+    });
+
+program
+    .command('build')
+    .alias('dist')
+    .description('构建模式')
+    .option('-m, --module [module]', 'The dev module')
+    .option('-e, --entry [entry]', 'The entry file')
+    .option('-t, --template [template]', 'The template html file')
+    .action(require('../lib/action/build'));
+
+
+program
+    .command('deploy')
+    .alias('prod')
+    .description('发布模式')
+    .option('-m, --module [module]', 'The dev module')
+    .option('-e, --entry [entry]', 'The entry file')
+    .option('-t, --template [template]', 'The template html file')
+    .action(require('../lib/action/prod'));
+
+program
+    .parse(process.argv);
